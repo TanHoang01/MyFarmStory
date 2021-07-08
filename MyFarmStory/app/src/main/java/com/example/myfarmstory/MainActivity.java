99e409247c363113ef4ae8fe897cbc36a8788696
TanHoang01 <manhtan12327@gmail.com>
2021-07-09 02:44:59 +0700

    Fix dialog

--- MyFarmStory/app/src/main/java/com/example/myfarmstory/MainActivity.java
+++ MyFarmStory/app/src/main/java/com/example/myfarmstory/MainActivity.java
@@ -489,7 +489,7 @@ public class MainActivity extends AppCompatActivity {
                                 refdatabase.child(USERS).child(auth.getCurrentUser().getUid()).setValue(users);
                                 newhighscoreDialog();
                             }
-                            else if(Integer.parseInt(lastpoint) > score){
+                            else if(Integer.parseInt(lastpoint) >= score){
                                 yourscoreDialog();
                             }
                         }
@@ -511,7 +511,7 @@ public class MainActivity extends AppCompatActivity {
                                 refdatabase.child(USERS).child(auth.getCurrentUser().getUid()).setValue(users);
                                 newhighscoreDialog();
                             }
-                            else if(Integer.parseInt(lastpoint) > score){
+                            else if(Integer.parseInt(lastpoint) >= score){
                                 yourscoreDialog();
                             }
                         }
